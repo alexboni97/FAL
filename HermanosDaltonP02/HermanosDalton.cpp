7d31ebed96573edaca5a091453b381d6d52c00d3
AlexBoni97 <alexboni@ucm.es>
2023-09-23 19:02:43 +0200

    modificacion hermanosdalton.cpp

--- HermanosDaltonP02/HermanosDalton.cpp
+++ HermanosDaltonP02/HermanosDalton.cpp
@@ -1,24 +1,57 @@
-// ALEX GUILLERMO BONILLA TACO
+// ALEX GUILLERMO BONILLA TACO 
 
 #include <iostream>
 
 #include <fstream>
 using namespace std;
+const int MAX_P = 100000;
+const int MAX_ALT = 1000000;
+
+
+bool esDalton(int vinetas[], const int & nPer) {
+    
+    if (nPer < 2||nPer>MAX_P|| vinetas[0] == vinetas[1]) return false;
+   
+    if (vinetas[0] < vinetas[1]) {
+        int i = 1;
+        while (i < nPer) {
+            if (vinetas[i - 1] > vinetas[i]||vinetas[i-1]<=0||vinetas[i]>MAX_ALT) return false;
+            i++;
+        }
+   
+    }
+    
+    else if (vinetas[0] > vinetas[1]) {
+        int i = 1;
+        while (i < nPer) {
+            if (vinetas[i - 1] < vinetas[i] || vinetas[i] <= 0|| vinetas[i-1] > MAX_ALT) return false;
+            i++;
+        }
+    }
+    
+    return true;
+}
+
 
 bool resuelveCaso() {
     int nPer;
-
+    int vinetas[MAX_P];
+    
     //Leer caso de prueba: cin>>...
     cin >> nPer;
     for (int i = 0; i < nPer; i++) {
-
+        cin >> vinetas[i];
     }
+
     if (!std::cin||nPer==0)
         return false;
 
-
     //Resolver problema
     //Escribir resultado
+    if (esDalton(vinetas, nPer)) cout << "DALTON\n" ;
+    else
+        cout << "DESCONOCIDOS\n";
+    
     return true;
 }
 
